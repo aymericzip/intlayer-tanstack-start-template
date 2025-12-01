308c2e535310ad9406867ebbd5091d241fc6808d
Aymeric PINEAU <ay.pineau@gmail.com>
2025-12-01 20:10:30 +0100

    style: format code for improved readability in vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,23 +1,33 @@
-import tailwindcss from "@tailwindcss/vite";
-import { tanstackStart } from "@tanstack/react-start/plugin/vite";
-import viteReact from "@vitejs/plugin-react";
-import { nitro } from "nitro/vite";
-import { defineConfig } from "vite";
-import { intlayer, intlayerProxy } from "vite-intlayer";
-import viteTsConfigPaths from "vite-tsconfig-paths";
+import tailwindcss from '@tailwindcss/vite';
+import { tanstackStart } from '@tanstack/react-start/plugin/vite';
+import viteReact from '@vitejs/plugin-react';
+import { nitro } from 'nitro/vite';
+import { defineConfig } from 'vite';
+import { intlayer, intlayerProxy } from 'vite-intlayer';
+import viteTsConfigPaths from 'vite-tsconfig-paths';
 
 const config = defineConfig({
-	plugins: [
-		nitro(),
-		viteTsConfigPaths({
-			projects: ["./tsconfig.json"],
-		}),
-		tailwindcss(),
-		tanstackStart(),
-		viteReact(),
-		intlayer(), // To make intlayer work
-		intlayerProxy(), // To redirect the user to his own locale
-	],
+  plugins: [
+    nitro(),
+    viteTsConfigPaths({
+      projects: ['./tsconfig.json'],
+    }),
+    tailwindcss(),
+    tanstackStart(),
+    viteReact(),
+    intlayer(), // To make intlayer work
+    intlayerProxy(), // To redirect the user to his own locale
+  ],
+  ssr: {
+    noExternal: [
+      'intlayer',
+      'react-intlayer',
+      '@intlayer/core',
+      '@intlayer/config',
+      '@intlayer/utils',
+      'vite-intlayer',
+    ],
+  },
 });
 
 export default config;
